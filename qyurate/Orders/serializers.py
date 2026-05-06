f0c31a7839ead019aba1884e4732511281bca23f
Antonykibet <antonykibet059@gmail.com>
2026-05-06 22:55:07 +0300

    feat: Return order items in Order GET requests

--- qyurate/Orders/serializers.py
+++ qyurate/Orders/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from Orders.models import Order, OrderItem
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__" 
-
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = "__all__" 
+        fields = "__all__"
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
